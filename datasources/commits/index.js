82b3b34e2e114bb83bfc306e8904d2fa3d72cbf6
Felix Riedl <felix1.riedl@stud.uni-regensburg.de>
2020-07-12 08:49:10 +0200

    Add/Update Component:   datasource.git commits

--- datasources/commits/index.js
+++ datasources/commits/index.js
@@ -7,32 +7,38 @@ module.exports = async function (localPath, token = undefined) {
         let commits = (await git.log()).all
         const hashes = commits.map(c => c.hash)
         let promises = []
-        
+
         commits.sort((a, b) =>
-            new Date(a.date) <= new Date(b.date) ? -1: 1
+            new Date(a.date) <= new Date(b.date) ? -1 : 1
         )
 
         const firstCommit = commits[0]
-        promises.push(new Promise(async function(resolve, reject) {
-         firstCommit.diffs = diffParse(await git.diff([firstCommit.hash], ['HEAD']))   
-         firstCommit.additions = firstCommit.diffs.reduce((a, diff) => a + diff.additions, 0)
-         firstCommit.deletions = firstCommit.diffs.reduce((a, diff) => a + diff.deletions, 0)
-         firstCommit.filesChanged = firstCommit.diffs.map(d => d.to)
-         resolve(firstCommit)
+        promises.push(new Promise(async function (resolve, reject) {
+            firstCommit.diffs = diffParse(await git.diff([firstCommit.hash], ['HEAD']))
+            firstCommit.additions = firstCommit.diffs.reduce((a, diff) => a + diff.additions, 0)
+            firstCommit.deletions = firstCommit.diffs.reduce((a, diff) => a + diff.deletions, 0)
+            firstCommit.filesChanged = firstCommit.diffs.map(d => d.to)
+            resolve(firstCommit)
         }))
-        
-        
+
+
         for (let i = 1; i < hashes.length; i++) {
             let c = commits[i]
-            let p = commits[i-1]
+            let p = commits[i - 1]
             promises.push(new Promise(async function (resolve, reject) {
                 const hash = c.hash
                 const commit = c
-                commit.diffs = diffParse(await git.diff([p.hash, hash]))
-                commit.additions = commit.diffs.reduce((a, diff) => a + diff.additions, 0)
-                commit.deletions = commit.diffs.reduce((a, diff) => a + diff.deletions, 0)
-                commit.filesChanged = commit.diffs.map(d => d.to)
-                resolve(commit)
+                try {
+                    let diffs = await git.diff([p.hash, hash])
+                    commit.diffs = diffParse(diffs)
+                    commit.additions = commit.diffs.reduce((a, diff) => a + diff.additions, 0)
+                    commit.deletions = commit.diffs.reduce((a, diff) => a + diff.deletions, 0)
+                    commit.filesChanged = commit.diffs.map(d => d.to)
+                    resolve(commit)
+                } catch(e) {
+                    console.error(e)
+                    reject(e)
+                }
             }))
         }
         commits = await Promise.all(promises)
