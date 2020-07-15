3bf018711456b73524b181830e3aa96588ac3dd7
Felix Riedl <feronimo92@gmail.com>
2020-07-15 15:55:17 +0200

    Add/Update Component:   datasource.git files

--- datasources/files/index.js
+++ datasources/files/index.js
@@ -2,26 +2,16 @@ const simpleGit = require('simple-git').gitP;
 const fs = require('fs')
 
 module.exports = async function (localPath, token = undefined) {
-  const git = simpleGit(localPath);
-  let r = (await git.raw(['ls-files'])).split("\n")
-  const promises = r.map(async (f) => {
-    return {
-      file: f,
-      stats: fs.statSync(localPath+'/'+f)
-    };
-  })
-  const sizes = await Promise.all(promises)
-
-  return new Promise((resolve, reject) => {
-    /**
-         * TODO: Implement your data acquisition logic here.
-         *
-         * You can use nodegit (pre-installed), as well as any other node library for this purpose.
-         * Make sure to install them properly via npm i before referencing them.
-         *
-         * Call 'resolve' with the results of the acquisition.
-         * */
-
+  return new Promise(async(resolve, reject) => {
+    const git = simpleGit(localPath);
+    let r = (await git.raw(['ls-files'])).split("\n")
+    const promises = r.map(async (f) => {
+      return {
+        file: f,
+        stats: fs.statSync(localPath+'/'+f)
+      };
+    })
+    const sizes = await Promise.all(promises)
     resolve(sizes)
   })
 }
