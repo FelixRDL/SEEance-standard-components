82bffe7893a472ff7743269cba271d939cc94ba5
Felix Riedl <feronimo92@gmail.com>
2020-07-15 19:54:59 +0200

    Add/Update Component:   analysis files-by-type

---
+++ analyses/files-by-type/index.js
@@ -0,0 +1,33 @@
+const groupBy = require('lodash').groupBy;
+
+module.exports = async function (input, config, visualisation) {
+  return new Promise((resolve, reject) => {
+    const filesByType = groupBy(input.files, (v) => v.file.split('.').splice(-1)[0]);
+    let results = Object.keys(filesByType).map((x) => {
+      return {
+        x: x,
+        y: filesByType[x].length
+      }
+    }).sort((a, b) => a.y >= b.y ? 1:-1)
+
+    if(config && config['max_number_of_results'] && results.length > config['max_number_of_results']) {
+      results = results.splice(-config['max_number_of_results'])
+    }
+
+    const xs = results.map(r => r.x)
+    const ys = results.map(r => r.y)
+
+
+
+    resolve(visualisation.plot([
+      {
+        x: xs,
+        y: ys,
+        type: 'bar',
+        name: 'Number of Files By Extension'
+      }
+    ], {
+      title: "Number of Files By Extension"
+    }));
+  })
+}
---
+++ analyses/files-by-type/test.js
@@ -0,0 +1,38 @@
+const fs = require('fs')
+const http = require('http')
+const core = require('seeance-analysis-core')
+const analysis = require('./index')
+const analysisPkg = JSON.parse(fs.readFileSync('package.json', 'utf-8'))
+const ComponentProvider = core.ComponentProvider
+
+runTest()
+
+async function runTest () {
+  const cp = await ComponentProvider({
+    customRepositories: []
+  })
+  const sourcesNames = core.getDependencies([], {
+    pkg: analysisPkg
+  })
+  await cp.init()
+  const datasources = await Promise.all(sourcesNames.map((n) => cp.getDatasourceByName(n)))
+  const result = await core.analyze(process.argv[2], process.argv[3], datasources, [], {
+    pkg: analysisPkg,
+    module: analysis,
+    config: {
+      'max_number_of_results': 15
+    }
+  })
+  // Solution by stackoverflow user JLeXanDR
+  // (https://stackoverflow.com/questions/35995273/how-to-run-html-file-using-node-js)
+  if (process.argv.includes('--serve')) {
+    console.log('ANALYSIS TEST: Serving analysis output on http://localhost:8080')
+    http.createServer(function (request, response) {
+      response.writeHeader(200, { 'Content-Type': 'text/html' })
+      response.write(result)
+      response.end()
+    }).listen(8080)
+  } else {
+    process.exit(0)
+  }
+}
