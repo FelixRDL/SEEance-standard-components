7e774313f0fc3f386d5810cae129c92ac0f74970
Felix Riedl <feronimo92@gmail.com>
2020-07-17 09:57:34 +0200

    Fix files datasource testing

--- datasources/files/test.js
+++ datasources/files/test.js
@@ -1,13 +1,28 @@
-const git = require('simple-git')
+
 const rimraf = require('rimraf')
 const script = require('./index')
+const fs = require('fs')
+const package = JSON.parse(fs.readFileSync('package.json', 'utf-8'))
 const path = './.tmp'
 const logger = require('seeance-analysis-core').log
+const analyze = require('seeance-analysis-core').analyze
+
+console.log(package)
 
 rimraf(path, () => {
-  git().clone(process.argv[2], path).then(async () => {
-    const result = await script(path)
-    console.log('DATASOURCE TEST:', result)
-    rimraf(path, () => {})
+  analyze(process.argv[2], process.argv[3], [
+    {
+      manifest: package.seeance,
+      package: package,
+      module: script
+    }
+  ], [], {
+    package: {
+      name: "TEST"
+    },
+    module: async function(input, config, vis) {
+      console.log(input)
+      return Promise.resolve()
+    }
   })
 })
