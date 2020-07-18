2a3859352f1d365105a3e22ad87ab8b5608d3072
Felix Riedl <feronimo92@gmail.com>
2020-07-18 17:48:18 +0200

    Fix unify blames

--- preprocessors/unify-authors-for-blame/index.js
+++ preprocessors/unify-authors-for-blame/index.js
@@ -1,11 +1,18 @@
 module.exports = async function (input, config) {
   return new Promise((resolve, reject) => {
     const mappings = {}
-    if (config && config.mappings) {
-      config.mappings.split(';').map(m => {
-        mappings[m.split(':')[0]] = m.split(':')[1]
-      })
+    function initializeDict () {
+      if (config && config.mappings) {
+        config.mappings.map(mapping => {
+          const properName = mapping.properName
+          const aliases = mapping.aliases.split(';')
+          aliases.map(alias => {
+            mappings[alias] = properName
+          })
+        })
+      }
     }
+    initializeDict()
     input.blame = input.blame.map(b => {
       b.linesPerAuthor = Object.keys(b.linesPerAuthor).reduce((acc, key) => {
         if (mappings[key]) {
