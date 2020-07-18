11a494691fd7d66cb627da37df7215bf69985913
Felix Riedl <feronimo92@gmail.com>
2020-07-18 16:02:59 +0200

    Add code survival chart

---
+++ analyses/code-survival/index.js
@@ -0,0 +1,27 @@
+module.exports = async function (input, config, visualisation) {
+  return new Promise((resolve, reject) => {
+    const distinctAuthors = Array.from(new Set(input.commits.map(c => c.author_name)))
+    const totalInsertionsAuthors = distinctAuthors.map(a => {
+      const commits = input.commits.filter(c => c.author_name === a)
+      return commits.reduce((acc, c) => c.additions+acc, 0)
+    })
+    const lines = input.blame.map(b => b.linesPerAuthor)
+    const linesAlivePerAuthor = distinctAuthors.map(a => lines.reduce((acc, b) => acc + (b[a] ? b[a] : 0), 0))
+    const survivalPerAuthor = distinctAuthors.map((a, i) => {
+      return linesAlivePerAuthor[i] / totalInsertionsAuthors[i]
+    })
+
+    console.log(distinctAuthors, linesAlivePerAuthor, totalInsertionsAuthors)
+
+    resolve(visualisation.plot([
+      {
+        x: distinctAuthors,
+        y: survivalPerAuthor,
+        type: 'bar',
+        name: 'Code Survival Rate Per Autor'
+      }
+    ], {
+      title: "Project Evolution in LOC"
+    }));
+  })
+}
