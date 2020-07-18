fe6f0addeba88bd13b27276ffa6f6c888bed0856
Felix Riedl <felix1.riedl@stud.uni-regensburg.de>
2020-07-18 09:47:38 +0200

    Implement author unification for git blame

---
+++ preprocessors/unify-authors-for-blame/index.js
@@ -0,0 +1,22 @@
+module.exports = async function (input, config) {
+  return new Promise((resolve, reject) => {
+    const mappings = {}
+    if (config && config.mappings) {
+      config.mappings.split(';').map(m => {
+        mappings[m.split(':')[0]] = m.split(':')[1]
+      })
+    }
+    input.blame = input.blame.map(b => {
+      b.linesPerAuthor = Object.keys(b.linesPerAuthor).reduce((acc, key) => {
+        if (mappings[key]) {
+          acc[mappings[key]] = b.linesPerAuthor[key]
+        } else {
+          acc[key] = b.linesPerAuthor[key]
+        }
+        return acc
+      }, {})
+      return b
+    })
+    resolve(input)
+  })
+}
---
+++ preprocessors/unify-authors-for-blame/test.js
@@ -0,0 +1,35 @@
+const fs = require('fs')
+const core = require('seeance-analysis-core')
+const preprocessor = require('./index')
+const preprocessorPkg = JSON.parse(fs.readFileSync('package.json', 'utf-8'))
+const ComponentProvider = core.ComponentProvider
+
+runTest().then(() => {
+  process.exit(0)
+})
+
+async function runTest () {
+  const cp = await ComponentProvider({
+    customRepositories: [],
+    reloadOnly: true
+  })
+  await cp.init()
+  const datasources = await Promise.all(['blame'].map((n) => cp.getDatasourceByName(n)))
+  return core.analyze(process.argv[2], process.argv[3], datasources, [{
+    package: preprocessorPkg,
+    module: preprocessor,
+    config: {
+      mappings: 'unknown:Johannes Dengler;Unknown:Johannes Dengler;PWinderl:Paul Winderl;FelixRDL:Felix Riedl'
+    }
+  }], {
+    config: {},
+    module: async (i, c, v) => {
+      console.log('TEST: PRINT RESULT')
+      console.log(JSON.stringify(i))
+      return Promise.resolve('success')
+    },
+    package: {
+      name: 'x'
+    }
+  })
+}
