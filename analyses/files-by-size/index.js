5d626aecd0c82699ab3d060a72161f68e7fcc676
Felix Riedl <felix1.riedl@stud.uni-regensburg.de>
2020-07-27 14:58:52 +0200

    Implement selection between lines and bytes

--- analyses/files-by-size/index.js
+++ analyses/files-by-size/index.js
@@ -1,27 +1,35 @@
 module.exports = async function (input, config, visualisation) {
   return new Promise((resolve, reject) => {
-    const files = input.files.sort((a,b) => a.stats.size >= b.stats.size ? 1:-1 );
+    const visualizeBy = (config && config.dimension) ? config.dimension : 'lines'
+    let files = input.files
+    let ys
+    if(visualizeBy === 'lines') {
+      files = files.sort((a, b) => a.lines >= b.lines ? 1 : -1)
+      ys = files.map((file) => file.lines)
+    } else {
+      files = files.sort((a, b) => a.stats.size >= b.stats.size ? 1 : -1)
+      ys = files.map((file) => file.stats.size)
+    }
     const xs = files.map((file) => file.file)
-    const ys = files.map((file) => file.stats.size)
     resolve(visualisation.plot([
       {
         x: xs,
         y: ys,
         type: 'bar',
-        name: 'File sizes'
+        name: `File sizes (${visualizeBy})`
       }
     ], {
-      title: "File sizes in Project",
+      title: `File sizes in Project (${visualizeBy})`,
       xaxis: {
         title: {
-          text: `Filename`
+          text: 'Filename'
         }
       },
       yaxis: {
         title: {
-          text: 'File size (Bytes)'
+          text: `${visualizeBy}`
         }
       }
-    }));
+    }))
   })
 }
--- analyses/files-by-size/test.js
+++ analyses/files-by-size/test.js
@@ -11,15 +11,13 @@ async function runTest () {
   const cp = await ComponentProvider({
     customRepositories: []
   })
-  const sourcesNames = core.getDependencies([], {
-    pkg: analysisPkg
-  })
+  const sourcesNames = analysisPkg.seeance.depends_on
   await cp.init()
   const datasources = await Promise.all(sourcesNames.map((n) => cp.getDatasourceByName(n)))
   const result = await core.analyze(process.argv[2], process.argv[3], datasources, [], {
-    pkg: analysisPkg,
+    package: analysisPkg,
     module: analysis,
-    config: analysisPkg.seeance
+    config: undefined
   })
   // Solution by stackoverflow user JLeXanDR
   // (https://stackoverflow.com/questions/35995273/how-to-run-html-file-using-node-js)
