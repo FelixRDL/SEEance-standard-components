16adc60fdca2e791f06ec41ec7c4247246568853
Felix Riedl <felix1.riedl@stud.uni-regensburg.de>
2020-07-27 15:43:04 +0200

    Implement author sorting

--- analyses/files-per-commit-number/index.js
+++ analyses/files-per-commit-number/index.js
@@ -1,12 +1,11 @@
 module.exports = async function (input, config, visualisation) {
   return new Promise((resolve, reject) => {
     const commitsForFiles = input.commits.reduce((acc, commit) => {
-      if(!commit.filesChanged)
-        return acc
-      const files = commit.filesChanged;
-      const result = acc;
+      if (!commit.filesChanged) { return acc }
+      const files = commit.filesChanged
+      const result = acc
       files.forEach((file) => {
-        if(!result[file.file]) {
+        if (!result[file.file]) {
           result[file.file] = [commit]
         } else {
           result[file.file].push(commit)
@@ -15,28 +14,43 @@ module.exports = async function (input, config, visualisation) {
       return result
     }, {})
 
-    let result = Object.keys(commitsForFiles).map((fileName) => {
-      return {
-        x: fileName,
-        y: commitsForFiles[fileName].length
+    const commitsByAuthors = input.commits.reduce((acc, commit) => {
+      if (!acc[commit.author_name]) {
+        acc[commit.author_name] = { files: {} }
       }
-    }).sort((a,b) => a.y >= b.y ? 1 : -1 )
-    if(config && config.max_number_of_results && result.length > config.max_number_of_results) {
-      result = result.splice(-(config['max_number_of_results']))
-    }
+      const authorStat = acc[commit.author_name]
+      commit.filesChanged.map((f) => {
+        if (!authorStat.files[f.file]) {
+          authorStat.files[f.file] = 0
+        }
+        authorStat.files[f.file] += 1
+      })
+      return acc
+    }, {})
 
-    resolve(visualisation.plot([
-      {
-        x: result.map((r) => r.x),
-        y: result.map((r) => r.y),
-        type: 'bar',
-        name: 'Commits for File'
+    const files = Object.keys(commitsForFiles).sort((k, i) => commitsForFiles[k].length - commitsForFiles[i].length)
+    const plots = [{
+      x: files,
+      y: files.map(f => 0),
+      type: 'bar',
+      name: 'total',
+      showlegend: false,
+      hoverinfo: 'none'
+    }].concat(Object.keys(commitsByAuthors).sort((a,b) => a <= b ? -1 : 1).map(k => {
+      return {
+        name: k,
+        x: Object.keys(commitsByAuthors[k].files),
+        y: Object.keys(commitsByAuthors[k].files).map(f => commitsByAuthors[k].files[f]),
+        type: 'bar'
       }
-    ], {
-      title: "Files per Commit Number",
+    }))
+
+    resolve(visualisation.plot(plots, {
+      barmode: 'stack',
+      title: 'Files per Commit Number',
       xaxis: {
         title: {
-          text: `Filename`
+          text: 'Filename'
         }
       },
       yaxis: {
@@ -44,6 +58,6 @@ module.exports = async function (input, config, visualisation) {
           text: 'Number of commits'
         }
       }
-    }));
+    }))
   })
 }
