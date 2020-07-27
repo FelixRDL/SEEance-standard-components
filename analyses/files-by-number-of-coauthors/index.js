df131342de3b70f88ffa5e83a6499721ed2ccde6
Felix Riedl <felix1.riedl@stud.uni-regensburg.de>
2020-07-27 13:53:29 +0200

    Implement workaround for sorting by number of authors

--- analyses/files-by-number-of-coauthors/index.js
+++ analyses/files-by-number-of-coauthors/index.js
@@ -1,13 +1,24 @@
 module.exports = async function (input, config, visualisation) {
   return new Promise((resolve, reject) => {
     const commits = input.commits
+    const authorsPerFile = {}
     const authorContribs = commits.reduce((acc, commit) => {
       const author = commit.author_name
       if (!acc[author]) { acc[author] = new Set() }
-      commit.filesChanged.forEach(fileChanged => acc[author].add(fileChanged.file))
+      commit.filesChanged.forEach(fileChanged => {
+        acc[author].add(fileChanged.file)
+        if (!authorsPerFile[fileChanged.file]) { authorsPerFile[fileChanged.file] = new Set() }
+        authorsPerFile[fileChanged.file].add(author)
+      })
       return acc
     }, {})
-    const authors = Object.keys(authorContribs).sort((a,b) => a <= b ? -1 : 1)
+    const numAuthorsPerFile = Object.keys(authorsPerFile).map((file) => {
+      return {
+        file: file,
+        numAuthors: authorsPerFile[file].size
+      }
+    }).sort((a, b) => a.numAuthors <= b.numAuthors ? -1 : 1)
+    const authors = Object.keys(authorContribs).sort((a, b) => a <= b ? -1 : 1)
     const plots = authors.map((a) => {
       return {
         x: Array.from(authorContribs[a]),
@@ -15,6 +26,13 @@ module.exports = async function (input, config, visualisation) {
         type: 'bar',
         name: a
       }
+    })
+    plots.splice(0, 0, {
+      x: numAuthorsPerFile.map(entry => entry.file),
+      y: numAuthorsPerFile.map(entry => 0),
+      type: 'bar',
+      name: 'total',
+      showlegend: false
     })
 
     resolve(visualisation.plot(plots, {
