9f796d1db776de31546eef065968c018cda4afca
Felix Riedl <felix1.riedl@stud.uni-regensburg.de>
2020-07-27 14:24:11 +0200

    Count lines in file datasource

--- datasources/files/index.js
+++ datasources/files/index.js
@@ -1,15 +1,26 @@
-const simpleGit = require('simple-git').gitP;
-const fs = require('fs');
+const simpleGit = require('simple-git').gitP
+const fs = require('fs')
 
 module.exports = async function (localPath, token = undefined) {
-  return new Promise(async(resolve, reject) => {
-    const git = simpleGit(localPath);
-    let files = (await git.raw(['ls-files'])).split("\n")
-    const s = await Promise.all(files.map(file => fs.promises.stat(localPath + '/' + file)))
-    const result = files.map((file, i) => {
+  return new Promise(async (resolve, reject) => {
+    const git = simpleGit(localPath)
+    const files = (await git.raw(['ls-files'])).split('\n')
+    let s = (await Promise.all(files.map(file => fs.promises.stat(localPath + '/' + file)))).map((s, i) => {
       return {
-        file: file,
-        stats: s[i]
+        file: files[i],
+        stats: s
+      }
+    })
+    s = s.filter((stat, i) => stat.stats.isFile())
+    const lines = (await Promise.all(s.map(item => {
+      return fs.promises.readFile(localPath + '/' + item.file, 'utf-8')
+    }))).map(content => content.split('\n').length)
+
+    const result = s.map((item, i) => {
+      return {
+        file: item.file,
+        stats: item.stats,
+        lines: lines[i]
       }
     })
     resolve(result)
