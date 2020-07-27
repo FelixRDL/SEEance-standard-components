a8c8bd107f1ab119743f4fa09928d6c661bff17f
Felix Riedl <felix1.riedl@stud.uni-regensburg.de>
2020-07-27 12:06:07 +0200

    Sort authors by name and add config label

--- analyses/file-ownership/index.js
+++ analyses/file-ownership/index.js
@@ -1,44 +1,43 @@
 module.exports = async function (input, config, visualisation) {
-  const blame = input.blame;
-  authors = blame.reduce((acc, b) => {
+  const blame = input.blame
+  const authorSet = blame.reduce((acc, b) => {
     const fileAuthors = Object.keys(b.linesPerAuthor)
     return new Set([...acc, ...new Set(fileAuthors)])
   }, new Set())
-    const dict = Array.from(authors).reduce((acc, a) => {
-        acc[a] = {
-            x: [],
-            y: [],
-            type: 'bar',
-            name: a
-        }
-        return acc
-    }, {})
-    blame.forEach((b) => {
-        const authors = Object.keys(b.linesPerAuthor)
-        authors.forEach(author => {
-            dict[author].x.push(b.file)
-            dict[author].y.push(b.linesPerAuthor[author])
-        })
+  const authors = Array.from(authorSet).sort((a, b) => a >= b ? -1 : 1)
+  const dict = authors.reduce((acc, a) => {
+    acc[a] = {
+      x: [],
+      y: [],
+      type: 'bar',
+      name: a
+    }
+    return acc
+  }, {})
+  blame.forEach((b) => {
+    const authors = Object.keys(b.linesPerAuthor)
+    authors.forEach(author => {
+      dict[author].x.push(b.file)
+      dict[author].y.push(b.linesPerAuthor[author])
     })
-    const result = Object.keys(dict).map(k => dict[k])
-
-
+  })
+  const result = Object.keys(dict).map(k => dict[k])
 
   return new Promise((resolve, reject) => {
     resolve(visualisation.plot(
-        result, {
-            title: "Ownership By File",
-            barmode: "stack",
-            xaxis: {
-                title: {
-                    text: `Filename`
-                }
-            },
-            yaxis: {
-                title: {
-                    text: 'Lines created by author'
-                }
-            }
-    }));
+      result, {
+        title: 'Ownership By File',
+        barmode: 'stack',
+        xaxis: {
+          title: {
+            text: 'Filename'
+          }
+        },
+        yaxis: {
+          title: {
+            text: 'Lines created by author'
+          }
+        }
+      }))
   })
 }
