cb251c62eedb4b372fae655700c99c4f946d63c4
Felix Riedl <felix1.riedl@stud.uni-regensburg.de>
2020-08-04 17:41:58 +0200

    Revert from promises to callback api

--- datasources/commits/index.js
+++ datasources/commits/index.js
@@ -1,24 +1,26 @@
-const simpleGit = require('simple-git').gitP
+const simpleGit = require('simple-git')
 
 module.exports = async function (localPath, token = undefined) {
   return new Promise(async (resolve, reject) => {
     const git = simpleGit(localPath)
-    let commits = (await git.log(['--stat', '--all'])).all
-    console.log("CommitSource", "RawLen", commits.length)
-    commits = commits.map((c) => {
-      const curr = c
-      if (c.diff) {
-        curr.additions = c.diff.insertions
-        curr.deletions = c.diff.deletions
-        curr.filesChanged = c.diff.files
-      } else {
-        curr.additions = 0
-        curr.deletions = 0
-        curr.filesChanged = []
-      }
-      return curr
-    }).sort((a, b) => a.date >= b.date ? 1 : -1)
-    console.log("CommitSource", "Len", commits.length)
-    resolve(commits)
+    git.log(['--stat', '--all'], (err, result) => {
+      let commits = result.all
+      console.log("CommitSource", "RawLen", commits.length)
+      commits = commits.map((c) => {
+        const curr = c
+        if (c.diff) {
+          curr.additions = c.diff.insertions
+          curr.deletions = c.diff.deletions
+          curr.filesChanged = c.diff.files
+        } else {
+          curr.additions = 0
+          curr.deletions = 0
+          curr.filesChanged = []
+        }
+        return curr
+      }).sort((a, b) => a.date >= b.date ? 1 : -1)
+      console.log("CommitSource", "Len", commits.length)
+      resolve(commits)
+    })
   })
 }
