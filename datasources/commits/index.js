c5ed6ffaf7e39a33e091d517c010157c7ebc3730
Felix Riedl <felix1.riedl@stud.uni-regensburg.de>
2020-08-04 12:01:22 +0200

    Add Logging to commit source

--- datasources/commits/index.js
+++ datasources/commits/index.js
@@ -1,22 +1,23 @@
-const simpleGit = require('simple-git').gitP;
+const simpleGit = require('simple-git').gitP
 
 module.exports = async function (localPath, token = undefined) {
-    return new Promise(async (resolve, reject) => {
-        const git = simpleGit(localPath)
-        let commits = (await git.log(['--stat'])).all
-        commits = commits.map((c) => {
-            const curr = c
-            if(c.diff) {
-                curr.additions = c.diff.insertions
-                curr.deletions = c.diff.deletions
-                curr.filesChanged = c.diff.files
-            } else {
-                curr.additions = 0
-                curr.deletions = 0
-                curr.filesChanged = []
-            }
-            return curr
-        }).sort((a,b) => a.date>=b.date ? 1 : -1)
-        resolve(commits)
-    })
+  return new Promise(async (resolve, reject) => {
+    const git = simpleGit(localPath)
+    let commits = (await git.log(['--stat'])).all
+    commits = commits.map((c) => {
+      const curr = c
+      if (c.diff) {
+        curr.additions = c.diff.insertions
+        curr.deletions = c.diff.deletions
+        curr.filesChanged = c.diff.files
+      } else {
+        curr.additions = 0
+        curr.deletions = 0
+        curr.filesChanged = []
+      }
+      return curr
+    }).sort((a, b) => a.date >= b.date ? 1 : -1)
+    console.log("CommitSource", commits)
+    resolve(commits)
+  })
 }
