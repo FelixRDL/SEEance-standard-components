e2c6e5c4adafa47be27f8fa58783821d35a9c1a1
Felix Riedl <felix1.riedl@stud.uni-regensburg.de>
2020-08-04 19:57:25 +0200

    Final fix

--- datasources/commits/index.js
+++ datasources/commits/index.js
@@ -1,4 +1,3 @@
-const simpleGit = require('simple-git')
 // Command Set by TextArcana https://gist.github.com/textarcana/1306223
 const diffTemplateCommand = `
      cd $LOCALPATH; \\
@@ -91,28 +90,8 @@ module.exports = async function (localPath, token = undefined) {
             filesChanged: fileDiffs
           }
         })
-
-        console.log(result)
+        resolve(result)
       })
     })
-
-    /* const git = simpleGit(localPath)
-        git.log(['--stat', '--all'], (err, result) => {
-      let commits = result.all
-      console.log("CommitSource", "RawLen", commits.length)
-      commits = commits.map((c) => {
-        const curr = c
-        if (c.diff) {
-          curr.additions = c.diff.insertions
-          curr.deletions = c.diff.deletions
-          curr.filesChanged = c.diff.files
-        } else {
-          curr.additions = 0
-          curr.deletions = 0
-          curr.filesChanged = []
-        }
-        return curr
-      }).sort((a, b) => a.date >= b.date ? 1 : -1)
-      console.log("CommitSource", "Len", commits.length) */
   })
 }
