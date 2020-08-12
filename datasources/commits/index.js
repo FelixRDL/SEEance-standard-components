8c16a664b7cee418dd405549a412e99274da7a43
Felix Riedl <felix1.riedl@stud.uni-regensburg.de>
2020-08-12 14:02:11 +0200

    Implement new diffparse

--- datasources/commits/index.js
+++ datasources/commits/index.js
@@ -1,39 +1,24 @@
-// Command Set by TextArcana https://gist.github.com/textarcana/1306223
-const diffTemplateCommand = `
-     cd $LOCALPATH; \\
-     git log \\
-    --numstat \\
-    --format='%H' \\
-    $@ | \\
-    perl -lawne '
-        if (defined $F[1]) {
-            print qq#{"insertions": "$F[0]", "deletions": "$F[1]", "path": "$F[2]"},#
-        } elsif (defined $F[0]) {
-            print qq#],\\n"$F[0]": [#
-        };
-        END{print qq#],#}' | \\
-    tail -n +2 | \\
-    perl -wpe 'BEGIN{print "{"}; END{print "}"}' | \\
-    tr '\\n' ' ' | \\
-    perl -wpe 's#(]|}),\\s*(]|})#$1$2#g' | \\
-    perl -wpe 's#,\\s*?}$#}#'
-`
+const exec = require('child_process').exec
+var parse = require('parse-diff')
+
 const diffLogCommand = `cd $LOCALPATH; \\
 git log \\
-    --pretty=format:'{%n  "commit": "%H",%n  "author": "%aN <%aE>",%n  "date": "%ad",%n  "message": "%f"%n},' \\
+    --pretty=format:'{%n  "hash": "%H",%n  "author": "%aN <%aE>",%n  "date": "%ad",%n  "message": "%f"%n},' \\
     $@ | \\
     perl -pe 'BEGIN{print "["}; END{print "]\\n"}' | \\
     perl -pe 's/},]/}]/'
 `
 
-function getDiffs (localPath) {
+const diffStatCommand = `cd $LOCALPATH;  \\
+git diff $STARTHASH $ENDHASH`
+//  | diffstat -C -f0 -m -k
+
+async function getLog (path) {
   return new Promise((resolve, reject) => {
-    const command = diffTemplateCommand.replace('$LOCALPATH', localPath)
-    require('child_process').exec(command, (error, stdout, stderr) => {
-      if (error) {
-        reject(error)
-      } else if (stderr) {
-        reject(stderr)
+    const cmd = diffLogCommand.replace(/\$LOCALPATH/gi, path)
+    exec(cmd, (err, stdout, stderr) => {
+      if (err || stderr) {
+        reject(err || stderr)
       } else {
         resolve(JSON.parse(stdout))
       }
@@ -41,57 +26,86 @@ function getDiffs (localPath) {
   })
 }
 
-function getLog (localPath) {
+function parseGitstat (gitStatStr) {
+  const parsed = summarizeDiff(parse(gitStatStr))
+  return parsed
+}
+
+function summarizeDiff (diff) {
+  const summary = diff.map(file => {
+    const fileOps = {
+      file: file.to,
+      oldFile: file.from,
+      insertions: 0,
+      deletions: 0,
+      modifications: 0
+    }
+    file.chunks.map((chunk) => {
+      if (chunk) {
+        const insertions = chunk.changes.filter(change => change.type === 'add').length
+        const deletions = chunk.changes.filter(change => change.type === 'del').length
+        if (insertions === deletions) {
+          fileOps.modifications += insertions
+        } else if (insertions > deletions) {
+          fileOps.modifications += deletions
+          fileOps.insertions += insertions - deletions
+        } else {
+          fileOps.modifications += insertions
+          fileOps.deletions += deletions - insertions
+        }
+      }
+    })
+    return fileOps
+  })
+  return summary
+}
+
+async function getDiffStat (path, commitA, commitB) {
   return new Promise((resolve, reject) => {
-    const command = diffLogCommand.replace('$LOCALPATH', localPath)
-    require('child_process').exec(command, (error, stdout, stderr) => {
-      if (error) {
-        reject(error)
-      } else if (stderr) {
-        reject(stderr)
+    const cmd = diffStatCommand.replace('$LOCALPATH', path)
+      .replace('$STARTHASH', commitA)
+      .replace('$ENDHASH', commitB)
+    exec(cmd, { maxBuffer: 1024 * 5096 }, (err, stdout, stderr) => {
+      if (err || stderr) {
+        reject(err || stderr)
       } else {
-        resolve(JSON.parse(stdout))
+        resolve(parseGitstat(stdout))
       }
     })
   })
 }
 
-module.exports = async function (localPath, token = undefined) {
-  return new Promise((resolve, reject) => {
-    getLog(localPath).then((logs) => {
-      getDiffs(localPath).then((diffs) => {
-        let result = logs.map((log) => {
-          log.diff = diffs[log.commit]
-          return log
+async function getDiffs (path) {
+  const log = (await getLog(path)).reverse()
+  const promises = log.map((commit, index) => {
+    return new Promise((resolve, reject) => {
+      const commitA = index === 0 ? '4b825dc642cb6eb9a060e54bf8d69288fbee4904' : log[index - 1].hash
+      const commitB = commit.hash
+      getDiffStat(path, commitA, commitB).then(diffs => {
+        resolve({
+          hash: commit.hash,
+          date: new Date(commit.date).toISOString(),
+          message: commit.message,
+          author_name: commit.author.split(' <')[0],
+          author_email: (commit.author.split(' <')[1]).split('>')[0],
+          diff: {
+            files: diffs,
+            insertions: diffs.reduce((a, d) => { return a + d.insertions }, 0),
+            modifications: diffs.reduce((a, d) => { return a + d.modifications }, 0),
+            deletions: diffs.reduce((a, d) => { return a + d.deletions }, 0),
+            changed: diffs.length
+          },
+          insertions: diffs.reduce((a, d) => { return a + d.insertions }, 0),
+          modifications: diffs.reduce((a, d) => { return a + d.modifications }, 0),
+          deletions: diffs.reduce((a, d) => { return a + d.deletions }, 0),
+          filesChanged: diffs
         })
-        result = result.map(item => {
-          const fileDiffs = item.diff.map((diff) => {
-            return {
-              file: diff.path,
-              insertions: parseInt(diff.insertions) || 0,
-              deletions: parseInt(diff.deletions) || 0
-            }
-          })
-          return {
-            hash: item.commit,
-            date: new Date(item.date).toISOString(),
-            message: item.message,
-            author_name: item.author.split(' <')[0],
-            author_email: (item.author.split(' <')[1]).split('>')[0],
-            diff: {
-              files: fileDiffs,
-              insertions: fileDiffs.reduce((a, d) => { return a + d.insertions }, 0),
-              deletions: fileDiffs.reduce((a, d) => { return a + d.deletions }, 0),
-              changed: fileDiffs.length
-            },
-            insertions: fileDiffs.reduce((a, d) => { return a + d.insertions }, 0),
-            deletions: fileDiffs.reduce((a, d) => { return a + d.deletions }, 0),
-            filesChanged: fileDiffs
-          }
-        })
-        console.log('L COMMIT DS LEN', result.length)
-        resolve(result)
       })
     })
   })
+  return await Promise.all(promises)
+}
+
+module.exports = async function (localPath, token = undefined) {
+  return getDiffs(localPath)
 }
