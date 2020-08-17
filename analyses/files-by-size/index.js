1beee879d5ab92405d9639082554cb4d0b78dfb1
Felix Riedl <felix1.riedl@stud.uni-regensburg.de>
2020-08-17 16:06:41 +0200

    Fix ticks on files-by-sizes

--- analyses/files-by-size/index.js
+++ analyses/files-by-size/index.js
@@ -1,18 +1,27 @@
+const TICK_TRUNCATE_MAX = 10
+
+function truncate (s, MAX_LENGTH) {
+  return s.length > MAX_LENGTH ? '...' + s.substring(s.length - MAX_LENGTH - 4, s.length) : s
+}
+
+function blameMapper (blame) {
+  return {
+    file: blame.file,
+    loc: Object.keys(blame.linesPerAuthor).reduce((acc, authorName) =>
+      acc + blame.linesPerAuthor[authorName], 0)
+  }
+}
+
 module.exports = async function (input, config, visualisation) {
   return new Promise((resolve, reject) => {
-    const accumulatedBlames = input.blame.map((blame) => {
-      return {
-        file: blame.file,
-        loc: Object.keys(blame.linesPerAuthor).reduce((acc, authorName) =>
-          acc + blame.linesPerAuthor[authorName], 0)
-      }
-    }).sort((a, b) => a.loc - b.loc)
+    const accumulatedBlames = input.blame.map(blameMapper).sort((a, b) => a.loc - b.loc < 0 ? -1 : 1)
     const ys = accumulatedBlames.map(blame => blame.loc)
     const xs = accumulatedBlames.map(blame => blame.file)
     resolve(visualisation.plot([
       {
         x: xs,
         y: ys,
+        text: xs,
         type: 'bar',
         name: 'File sizes (LOC)'
       }
@@ -21,7 +30,10 @@ module.exports = async function (input, config, visualisation) {
       xaxis: {
         title: {
           text: 'Filename'
-        }
+        },
+        tickvals: xs,
+        ticktext: xs.map(x => truncate(x, TICK_TRUNCATE_MAX)),
+        automargin: true
       },
       yaxis: {
         title: {
