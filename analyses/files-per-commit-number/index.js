20db4863a5cc35f99029def9c8079ffe39e7b512
Felix Riedl <felix1.riedl@stud.uni-regensburg.de>
2020-08-17 16:21:43 +0200

    Minor improvements on files-per-commit-number

--- analyses/files-per-commit-number/index.js
+++ analyses/files-per-commit-number/index.js
@@ -1,3 +1,31 @@
+function commitsByFilesReducer (acc, commit) {
+  if (!commit.filesChanged) { return acc }
+  const files = commit.filesChanged
+  const result = acc
+  files.forEach((file) => {
+    if (!result[file.file]) {
+      result[file.file] = [commit]
+    } else {
+      result[file.file].push(commit)
+    }
+  })
+  return result
+}
+
+function commitsByFilesAndAuthorsMapper (acc, commit) {
+  if (!acc[commit.author_name]) {
+    acc[commit.author_name] = { files: {} }
+  }
+  const authorStat = acc[commit.author_name]
+  commit.filesChanged.map((f) => {
+    if (!authorStat.files[f.file]) {
+      authorStat.files[f.file] = 0
+    }
+    authorStat.files[f.file] += 1
+  })
+  return acc
+}
+
 module.exports = async function (input, config, visualisation) {
   return new Promise((resolve, reject) => {
     const standardColors = [
@@ -13,40 +41,17 @@ module.exports = async function (input, config, visualisation) {
       '#17becf'
     ]
 
-    const commitsForFiles = input.commits.reduce((acc, commit) => {
-      if (!commit.filesChanged) { return acc }
-      const files = commit.filesChanged
-      const result = acc
-      files.forEach((file) => {
-        if (!result[file.file]) {
-          result[file.file] = [commit]
-        } else {
-          result[file.file].push(commit)
-        }
-      })
-      return result
-    }, {})
+    const commitsForFiles = input.commits.reduce(commitsByFilesReducer, {})
     if (commitsForFiles['/dev/null']) {
       delete commitsForFiles['/dev/null']
     }
 
-    const commitsByAuthors = input.commits.reduce((acc, commit) => {
-      if (!acc[commit.author_name]) {
-        acc[commit.author_name] = { files: {} }
-      }
-      const authorStat = acc[commit.author_name]
-      commit.filesChanged.map((f) => {
-        if (!authorStat.files[f.file]) {
-          authorStat.files[f.file] = 0
-        }
-        authorStat.files[f.file] += 1
-      })
-      return acc
-    }, {})
+    const commitsByAuthors = input.commits.reduce(commitsByFilesAndAuthorsMapper, {})
 
     const files = Object.keys(commitsForFiles)
       .sort((k, i) => commitsForFiles[k].length - commitsForFiles[i].length)
       .filter(f => f !== '/dev/null')
+
     const plots = [{
       x: files,
       y: files.map(f => 0),
@@ -55,10 +60,11 @@ module.exports = async function (input, config, visualisation) {
       showlegend: false,
       hoverinfo: 'none'
     }].concat(Object.keys(commitsByAuthors).sort((a, b) => a.toLowerCase() <= b.toLowerCase() ? -1 : 1).map((k, i) => {
+      const xs = Object.keys(commitsByAuthors[k].files)
+        .filter(f => f !== '/dev/null')
       return {
         name: k,
-        x: Object.keys(commitsByAuthors[k].files)
-          .filter(f => f !== '/dev/null'),
+        x: xs,
         y: Object.keys(commitsByAuthors[k].files)
           .filter(f => f !== '/dev/null')
           .map(f => commitsByAuthors[k].files[f]),
@@ -75,7 +81,8 @@ module.exports = async function (input, config, visualisation) {
       xaxis: {
         title: {
           text: 'Filename'
-        }
+        },
+        automargin: true
       },
       yaxis: {
         title: {
