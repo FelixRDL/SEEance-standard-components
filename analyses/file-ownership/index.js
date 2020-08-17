43df504d971ba369e7dace644f5721179af0ed6e
Felix Riedl <felix1.riedl@stud.uni-regensburg.de>
2020-08-17 15:53:38 +0200

    Sort ownership by file by size, also fix xaxis

--- analyses/file-ownership/index.js
+++ analyses/file-ownership/index.js
@@ -1,27 +1,47 @@
+const TICK_TRUNCATE_MAX = 10
+
+function truncate (s, MAX_LENGTH) {
+  return s.length > MAX_LENGTH ? '...' + s.substring(s.length - MAX_LENGTH - 4, s.length) : s
+}
+
+function authorSetReducer (acc, blame) {
+  const fileAuthors = Object.keys(blame.linesPerAuthor)
+  return new Set([...acc, ...new Set(fileAuthors)])
+}
+
+function authorToTraceReducer (acc, authorName) {
+  acc[authorName] = {
+    x: [],
+    y: [],
+    type: 'bar',
+    text: [],
+    name: authorName
+  }
+  return acc
+}
+
+function linesForAllKeysCounter (dict) {
+  return Object.keys(dict).reduce((acc, key) => acc + dict[key], 0)
+}
+
 module.exports = async function (input, config, visualisation) {
   const blame = input.blame
-  const authorSet = blame.reduce((acc, b) => {
-    const fileAuthors = Object.keys(b.linesPerAuthor)
-    return new Set([...acc, ...new Set(fileAuthors)])
-  }, new Set())
+  const authorSet = blame.reduce(authorSetReducer, new Set())
   const authors = Array.from(authorSet).sort((a, b) => a.toLowerCase() <= b.toLowerCase() ? -1 : 1)
-  const dict = authors.reduce((acc, a) => {
-    acc[a] = {
-      x: [],
-      y: [],
-      type: 'bar',
-      name: a
-    }
-    return acc
-  }, {})
+  const dict = authors.reduce(authorToTraceReducer, {})
   blame.forEach((b) => {
     const authors = Object.keys(b.linesPerAuthor)
     authors.forEach(author => {
       dict[author].x.push(b.file)
+      dict[author].text.push(b.file)
       dict[author].y.push(b.linesPerAuthor[author])
     })
   })
   const result = Object.keys(dict).map(k => dict[k])
+  const tickvals = blame.sort((blameA, blameB) => {
+    return linesForAllKeysCounter(blameA.linesPerAuthor) - linesForAllKeysCounter(blameB.linesPerAuthor) < 0 ? 1 : -1
+  }).map(blame => blame.file)
+  const ticktexts = tickvals.map(s => truncate(s, TICK_TRUNCATE_MAX))
 
   return new Promise((resolve, reject) => {
     resolve(visualisation.plot(
@@ -29,9 +49,14 @@ module.exports = async function (input, config, visualisation) {
         title: 'Ownership By File',
         barmode: 'stack',
         xaxis: {
+          automargin: true,
           title: {
             text: 'Filename'
-          }
+          },
+          tickvals: tickvals,
+          ticktext: ticktexts,
+          categoryarray: tickvals,
+          categoryorder: 'array'
         },
         yaxis: {
           title: {
